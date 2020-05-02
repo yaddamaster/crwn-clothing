e6447569aa6e0ceb0f9a762d57dcc089fbafa3ea
yaddamaster <subscriptions@patrickhome.com>
2020-05-02 13:13:47 -0700

    integrated firebase into application, created sign in page with sign in component and sign in with google

--- src/App.js
+++ src/App.js
@@ -2,20 +2,51 @@ import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component.jsx';
+import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header-component';
+import { auth } from './firebase/firebase.utils';
 
-function App() {
-  return (
-    <div>
-    <Header />
-      <Switch>
-          <Route exact path='/' component={HomePage} />
-          <Route path='/shop' component={ShopPage} />
-          {/*<div><HomePage /></div>*/}
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  componentDidMount(){
+      this.unsubscribeFromAuth = auth.onAuthStateChanged(user => 
+        {
+          this.setState(
+            { 
+              currentUser: user 
+            }
+          );
+
+          console.log(user);
+      }
+    )
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render () {
+    return (
+      <div>
+      <Header  currentUser={this.state.currentUser} />
+        <Switch>
+            <Route exact path='/' component={HomePage} />
+            <Route path='/shop' component={ShopPage} />
+            <Route path='/signin' component={SignInAndSignUpPage} />
+            {/*<div><HomePage /></div>*/}
+        </Switch>
+      </div>
+      );
+  }
 }
 
 export default App;
---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,25 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyBcNEHFx27k6zaqO5dgV3PdZQdGqhaCsrc",
+    authDomain: "crwn-db-da56b.firebaseapp.com",
+    databaseURL: "https://crwn-db-da56b.firebaseio.com",
+    projectId: "crwn-db-da56b",
+    storageBucket: "crwn-db-da56b.appspot.com",
+    messagingSenderId: "1812080544",
+    appId: "1:1812080544:web:f8207e63e194c6f736e6c5"
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+// google auth
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
